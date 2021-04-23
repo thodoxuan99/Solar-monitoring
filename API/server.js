28f1d249688d92b9e622a339f327046d21732bb7
thodoxuan99 <tho.dok2017@hcmut.edu.vn>
2021-04-23 23:38:40 +0700

    setup API

--- API/server.js
+++ API/server.js
@@ -1,18 +1,35 @@
 import('../suncalc/suncalc.js');
- var time = require('time');
-
 const http = require('http');
 const SunCalc = require('../suncalc/suncalc.js');
+// const{ DateTime } = require('luxon');
 const { getPosition } = require('../suncalc/suncalc.js');
-console.log(new Date(+7));
-console.log(getPosition(new Date().toLocaleString("en-US", {timeZone:  'Asia/Ho_Chi_Minh'}),10.816572, 106.674488 ));
+// console.log(getPosition(new Date().toLocaleString("en-US", {timeZone:  'Asia/Ho_Chi_Minh'}),10.816572, 106.674488 ));
 const hostname = '127.0.0.1';
 const port = 3000;
+// const lat = 10.816572
+// const lng = 106.674488
+// console.log(convertUTCDateToLocalDate(new Date(Date.now())))
+// var date = convertUTCDateToLocalDate(new Date(Date.now()))
+// var position = SunCalc.getPosition(new Date())
+// console.log(position)
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  let data = '';
+  req.on('data', chunk => {
+    data += chunk;
+  })
+  req.on('end', () => {
+    console.log(data)
+    var json = JSON.parse(data)
+    var lat = json.lat
+    var lng = json.lng
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    var position = SunCalc.getPosition(new Date(),lat, lng)
+    res.end(JSON.stringify(position));
+  })
+  
+  
 });
 
 server.listen(port, hostname, () => {
